c6bae8bf5d0046fc3c99c39f09a326dc3132704b
Yuki Jinnouchi <yuki.jinnouchi@gmail.com>
2025-01-28 19:31:33 +0900

    update ex03 not to use getter and setter

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,21 +6,22 @@
 /*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 16:31:37 by yjinnouc          #+#    #+#             */
-/*   Updated: 2025/01/28 13:37:08 by yjinnouc         ###   ########.fr       */
+/*   Updated: 2025/01/28 19:29:21 by yjinnouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 // Orthodox Canonical Form
-DiamondTrap::DiamondTrap(){
+DiamondTrap::DiamondTrap()
+  : ClapTrap(), ScavTrap(), FragTrap(){
   std::cout \
     << "DiamondTrap default constructor called." << std::endl;
 }
 
 DiamondTrap::~DiamondTrap() {
   std::cout \
-    << "DiamondTrap " << getName() \
+    << "DiamondTrap " << name \
     << " destructor called."  << std::endl;
 }
 
@@ -28,37 +29,41 @@ DiamondTrap::DiamondTrap(DiamondTrap const &src)
   : ClapTrap(src), ScavTrap(src), FragTrap(src){
   std::cout \
     << "DiamondTrap copy constructor" \
-    << " from " << src.getName() \
+    << " from " << src.name \
     << " called." << std::endl;
-  ClapTrap::setName(src.getName() + "_clap_name");
-  setName(src.getName());
+  DiamondTrap::name = src.name;
+  ClapTrap::name = src.name + "_clap_name";
+  this->hitPoints = src.hitPoints;
+  this->energyPoints = src.energyPoints;
+  this->attackDamage = src.attackDamage;
 }
 
 DiamondTrap &DiamondTrap::operator=(const DiamondTrap &rhs){
   std::cout \
     << "DiamondTrap assignation operator" \
-    << " from " << rhs.getName() \
+    << " from " << rhs.name \
     << " called." << std::endl;
   if (this == &rhs)
     return *this;
-  ClapTrap::setName(rhs.getName() + "_clap_name");
-  setName(rhs.getName());
-  setHitPoints(rhs.getHitPoints());
-  setEnergyPoints(rhs.getEnergyPoints());
-  setAttackDamage(rhs.getAttackDamage());
+  DiamondTrap::name = rhs.name;
+  ClapTrap::name = rhs.name + "_clap_name";
+  this->hitPoints = rhs.hitPoints;
+  this->energyPoints = rhs.energyPoints;
+  this->attackDamage = rhs.attackDamage;
   return *this;
 }
 
 // Optional constructor
 DiamondTrap::DiamondTrap(std::string name)
-  : ClapTrap(name + "_clap_name"), ScavTrap(), FragTrap(){
+  : ClapTrap(name), ScavTrap(name), FragTrap(name) {
   std::cout \
     << "DiamondTrap constructor called" \
     << " with the name of " << name << std::endl;
-  setName(name);
-  setHitPoints(FragTrap::getHitPoints());
-  setEnergyPoints(ScavTrap::getEnergyPoints());
-  setAttackDamage(FragTrap::getAttackDamage());
+  DiamondTrap::name = name;
+  ClapTrap::name = name + "_clap_name";
+  DiamondTrap::hitPoints = FragTrap::hitPoints;
+  DiamondTrap::energyPoints = ScavTrap::energyPoints;
+  DiamondTrap::attackDamage = FragTrap::attackDamage;
 }
 
 // Getters and Setters
@@ -77,7 +82,7 @@ void DiamondTrap::attack(const std::string& target){
 
 void DiamondTrap::whoAmI(void) const {
   std::cout \
-    << "My name is " << getName() << ", " \
-    << "and my ClapTrap name is " << ClapTrap::getName() << std::endl;
+    << "My name is " << name << ", " \
+    << "and my ClapTrap name is " << ClapTrap::name << std::endl;
 }
 
